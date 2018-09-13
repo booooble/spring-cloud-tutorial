9d684151481ad1de1a69ba63f87321540e18b747
Temp Name <tony_l@ua-gn-9999923.local>
2018-09-13 15:48:19 +0300

    part 4

---
+++ spring-cloud-bootstrap/rating-service/src/main/java/com/example/ratingservice/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.example.ratingservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@Order(1)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("password")
+                .roles("USER").and().withUser("admin").password("admin")
+                .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().disable().authorizeRequests()
+                .antMatchers("/ratings").hasRole("USER")
+                .antMatchers("/ratings/all").hasAnyRole("USER", "ADMIN").anyRequest()
+                .authenticated().and().csrf().disable();
+    }
+
+    @Configuration
+    public static class AdminSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER)
+                    .and().httpBasic().disable().authorizeRequests()
+                    .antMatchers(HttpMethod.GET, "/").hasRole("ADMIN")
+                    .antMatchers("/info", "/health").authenticated().anyRequest()
+                    .denyAll().and().csrf().disable();
+        }
+    }
+}
